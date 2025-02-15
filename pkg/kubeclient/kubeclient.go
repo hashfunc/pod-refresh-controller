2bdf96ca5e4fdf43d914248f0f0c375666378266
hashfunc <root@hashfunc.io>
2025-02-15 22:40:58 +0900

    Add kubeclient

---
+++ pkg/kubeclient/kubeclient.go
@@ -0,0 +1,43 @@
+package kubeclient
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func New(enableLocalConfig bool) (kubernetes.Interface, error) {
+	var config *rest.Config
+
+	if enableLocalConfig {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			return nil, err
+		}
+
+		kubeconfig := path.Join(homeDir, ".kube", "config")
+
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			return nil, fmt.Errorf("building from local config: %w", err)
+		}
+	} else {
+		var err error
+
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			return nil, fmt.Errorf("building in-cluster config: %w", err)
+		}
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("creating client: %w", err)
+	}
+
+	return client, nil
+}

2b150e433716d855c5aeeac30d886ed809ed901a
hashfunc <root@hashfunc.io>
2025-02-16 00:03:18 +0900

    Update controller to manage deployments

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -4,16 +4,21 @@ import (
 	"fmt"
 	"time"
 
-	corev1 "k8s.io/api/core/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
+	corelisters "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/klog/v2"
 )
 
 type Controller struct {
+	kubeclient            kubernetes.Interface
 	sharedInformerFactory informers.SharedInformerFactory
+	podLister             corelisters.PodLister
 	podsSynced            cache.InformerSynced
+	deploymentsSynced     cache.InformerSynced
 }
 
 func NewController(kubeclient kubernetes.Interface, namespace string) *Controller {
@@ -24,28 +29,52 @@ func NewController(kubeclient kubernetes.Interface, namespace string) *Controlle
 	)
 
 	podInformer := sharedInformerFactory.Core().V1().Pods()
+	deploymentInformer := sharedInformerFactory.Apps().V1().Deployments()
 
 	controller := &Controller{
+		kubeclient:            kubeclient,
 		sharedInformerFactory: sharedInformerFactory,
+		podLister:             podInformer.Lister(),
 		podsSynced:            podInformer.Informer().HasSynced,
+		deploymentsSynced:     deploymentInformer.Informer().HasSynced,
 	}
 
-	podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		UpdateFunc: func(_, updated interface{}) {
-			if pod, ok := updated.(*corev1.Pod); ok {
-				klog.Infof("Pod updated: %s/%s", pod.Namespace, pod.Name)
-			}
-		},
+	deploymentInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		UpdateFunc: controller.UpdateFuncDeployment,
 	})
 
 	return controller
 }
 
+func (c *Controller) UpdateFuncDeployment(oldObj, newObj interface{}) {
+	deployment, ok := newObj.(*appsv1.Deployment)
+	if !ok {
+		klog.Errorf("casting to deployment: %T", newObj)
+		return
+	}
+
+	selector, err := metav1.LabelSelectorAsSelector(deployment.Spec.Selector)
+	if err != nil {
+		klog.Errorf("parsing selector: %s", err)
+		return
+	}
+
+	pods, err := c.podLister.Pods(deployment.Namespace).List(selector)
+	if err != nil {
+		klog.Errorf("listing pods for %s: %s", deployment.Name, err)
+		return
+	}
+
+	for _, pod := range pods {
+		klog.Infof("%s created at %s", pod.Name, pod.CreationTimestamp)
+	}
+}
+
 func (controller *Controller) Run(stopCh <-chan struct{}) error {
 	controller.sharedInformerFactory.Start(stopCh)
 
-	if ok := cache.WaitForCacheSync(stopCh, controller.podsSynced); !ok {
-		return fmt.Errorf("waiting for cache sync")
+	if ok := cache.WaitForCacheSync(stopCh, controller.podsSynced, controller.deploymentsSynced); !ok {
+		return fmt.Errorf("failed to wait for caches to sync")
 	}
 
 	<-stopCh
